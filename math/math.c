b22d45341e8a905439205e5173143a060534fbac
Meenaksh Singhania <meenaksh.s23csai@nst.rishihood.edu.in>
2026-04-12 18:33:01 +0530

    math.h library

---
+++ math/math.c
@@ -0,0 +1,37 @@
+#include "math.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int multiply(int a, int b) {
+    int result = 0;
+    for (int i = 0; i < b; i++) {
+        result += a;
+    }
+    return result;
+}
+
+int divide(int a, int b) {
+    if (b == 0) return 0;
+
+    int count = 0;
+    while (a >= b) {
+        a -= b;
+        count++;
+    }
+    return count;
+}
+
+int mod(int a, int b) {
+    if (b == 0) return 0;
+
+    while (a >= b) {
+        a -= b;
+    }
+    return a;
+}
+
+int abs_val(int x) {
+    return x < 0 ? -x : x;
+}
---
+++ math/math.h
@@ -0,0 +1,10 @@
+#ifndef MATH_H
+#define MATH_H
+
+int add(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
+int mod(int a, int b);
+int abs_val(int x);
+
+#endif
